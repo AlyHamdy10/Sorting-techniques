8cc7cb245eedb2bf2ceadaae849ac9a38c8980cf
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2022-03-23 19:27:16 +0200

    Update find_kth_element.py
    error for some values of k

--- find_kth_element.py
+++ find_kth_element.py
@@ -1,5 +1,18 @@
-def partition(arr, p, r, k):
-    pass
+def partition(arr, p, r, k):  # fn using last element as pivot
+    x = arr[r]  # r is the index of the last element
+    i = p - 1
+    for j in range(p, r):
+        if arr[j] < x:
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+            print(arr)
+    arr[i+1], arr[r] = arr[r], arr[i+1]
+    if (i+1) == k:
+        return arr[i]
+    elif (i+1) > k:
+        return partition(arr, p, i, k)
+    else:
+        return partition(arr, i+2, r, k)
 
 
 k = int(input("Enter K"))
