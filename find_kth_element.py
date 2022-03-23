8bb31af613f183666d71e41972045ec1aa4697be
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2022-03-23 19:03:19 +0200

    Outline for find_kth_element

---
+++ find_kth_element.py
@@ -0,0 +1,11 @@
+def partition(arr, p, r, k):
+    pass
+
+
+k = int(input("Enter K"))
+arr = [6, 0, 7, 3, 1, 5, 9, 8, 4, 2]
+size = 10
+print(arr)
+print(partition(arr, 0, size-1, k))
+arr.sort()
+print(arr)
